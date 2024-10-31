761ef9d5e07905abedff4021fc8d18a357c74cc5
hwk9407 <hwk__@naver.com>
2024-10-31 18:00:04 +0900

    init commit

---
+++ src/main/java/com/sparta/challengeoauth/ChallengeOauthApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.challengeoauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeOauthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChallengeOauthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/challengeoauth/ChallengeOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.challengeoauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeOauthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
